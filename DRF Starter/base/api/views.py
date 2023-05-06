22499890780f1d015979fb02b577beb06f707727
Arsh2 <pathanarshkhan@gmail.com>
2023-05-06 11:18:35 +0530

    Added starter file for DRF

---
+++ DRF Starter/base/api/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        "api/token",
+        "api/token/refresh"
+    ]
+    return Response(routes)
